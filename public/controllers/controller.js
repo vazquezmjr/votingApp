55eb9973993e12be79436d36d3ae4b65f679726a
vazquezmjr <vazquezmjr@gmail.com>
2016-11-15 09:37:29 -0800

    Added server

---
+++ public/controllers/controller.js
@@ -0,0 +1,57 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http',function($scope,$http){
+  console.log("Hello World from controller");
+
+
+var editClicked = false;
+var refresh = function(){
+  $http.get('/contactlist').success(function(response){
+    console.log("I got the data i requested");
+    $scope.contactlist = response;
+    $scope.contact = "";
+  });
+};
+
+refresh();
+
+$scope.addContact = function() {
+  if(editClicked == false){
+  console.log($scope.contact);
+  $http.post('/contactlist', $scope.contact).success(function(response){
+    console.log(response);
+    refresh();
+  });
+}
+else {
+  alert("Click the clear button in order to add a new contact");
+}
+};
+
+$scope.remove = function(id){
+  console.log(id);
+  $http.delete('/contactlist/' + id).success(function(response){
+    refresh();
+  });
+};
+
+$scope.edit = function(id){
+  editClicked = true;
+  console.log(id);
+  $http.get('/contactlist/' + id).success(function(response){
+    $scope.contact = response;
+  });
+};
+
+$scope.update = function() {
+  console.log($scope.contact._id);
+  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+  refresh();
+  });
+};
+
+$scope.deselect = function(){
+  editClicked = false;
+  $scope.contact = "";
+}
+
+}])
